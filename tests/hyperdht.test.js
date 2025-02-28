f66c204b9e155ecccf25990a1da8aa292e514e6d
zacharygriffee <zacharygriffee@gmail.com>
2025-02-28 03:02:53 -0500

    fix test not supposed to be committed

--- tests/hyperdht.test.js
+++ tests/hyperdht.test.js
@@ -1,36 +1,36 @@
-import HyperDHT from "hyperdht";
-import { test, solo } from "brittle";
-import {createDHTClient, createDHTServer} from "../lib/hyperdht/index.js";
-import b4a from "b4a";
-import {map, mergeAll, take} from "rxjs";
-
-const testKeys = HyperDHT.keyPair();
+// import HyperDHT from "hyperdht";
+// import { test, solo } from "brittle";
+// import {createDHTClient, createDHTServer} from "../lib/hyperdht/index.js";
+// import b4a from "b4a";
+// import {map, mergeAll, take} from "rxjs";
 //
-// solo("Basic", async t => {
-//     t.plan(2);
-//     const dht = new HyperDHT();
-//     const { connections, listening } = createDHTServer(dht, {keyPair: testKeys});
-//
-//     const sub = connections.pipe(map(({newSockets}) => newSockets), mergeAll(), mergeAll()).subscribe({
-//         next: data => {
-//             t.is(`${data}`, "Hello");
-//             sub.complete();
-//         },
-//         complete: () => t.pass("Complete was called")
-//     });
-//
-//     await listening;
-//
-//     const { connection } = createDHTClient(dht, testKeys.publicKey);
-//
-//     connection.subscribe(stream => {
-//         stream.write(b4a.from("Hello"))
-//     })
-//
-//     t.teardown(() => {
-//         connections.complete();
-//         connection.complete();
-//         sub.unsubscribe();
-//         dht.destroy();
-//     });
-// });
+// const testKeys = HyperDHT.keyPair();
+// //
+// // solo("Basic", async t => {
+// //     t.plan(2);
+// //     const dht = new HyperDHT();
+// //     const { connections, listening } = createDHTServer(dht, {keyPair: testKeys});
+// //
+// //     const sub = connections.pipe(map(({newSockets}) => newSockets), mergeAll(), mergeAll()).subscribe({
+// //         next: data => {
+// //             t.is(`${data}`, "Hello");
+// //             sub.complete();
+// //         },
+// //         complete: () => t.pass("Complete was called")
+// //     });
+// //
+// //     await listening;
+// //
+// //     const { connection } = createDHTClient(dht, testKeys.publicKey);
+// //
+// //     connection.subscribe(stream => {
+// //         stream.write(b4a.from("Hello"))
+// //     })
+// //
+// //     t.teardown(() => {
+// //         connections.complete();
+// //         connection.complete();
+// //         sub.unsubscribe();
+// //         dht.destroy();
+// //     });
+// // });
